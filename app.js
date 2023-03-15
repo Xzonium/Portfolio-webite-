9a939ec884ff20f8eb09005d2ef463901d66701f
Xzonium <123448625+Xzonium@users.noreply.github.com>
2023-03-15 17:59:00 +0000

    Add files via upload

--- app.js
+++ app.js
@@ -5,3 +5,16 @@ menu.addEventListener('click', function(){
     menu.classList.toggle('is-active');
     menuLinks.classList.toggle('active');
 });
+
+//links for projects 
+function openIt () {
+    const url = 'https://lol-shop-g2g.netlify.app'
+    window.open (url);
+}
+
+// preloader
+// var loader =document.getElementById("preLoader");
+
+// window.addEventListener("load", function(){
+//     loader.style.display = "none";
+// })
